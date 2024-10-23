a9923cf822639df6138ed5d9ebd8ef83852b2e2c
Harsh <harshmandalilya315@gmail.com>
2024-10-24 00:06:55 +0530

    scanning bluetooth devices functionality added

--- lib/Scanned.dart
+++ lib/Scanned.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_blue/flutter_blue.dart';
 import 'package:get/get.dart';
 import 'package:sample_logins/Services/ble_controller.dart';
 
@@ -26,31 +25,26 @@ class _ScannedState extends State<Scanned> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Expanded(
-                  child: StreamBuilder<List<ScanResult>>(
-                    stream: controller.scanResults,
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-                        return ListView.builder(
-                          itemCount: snapshot.data!.length, // Define item count here
-                          itemBuilder: (context, index) {
-                            final data = snapshot.data![index];
-                            return Card(
-                              elevation: 2,
-                              child: ListTile(
-                                title: Text(data.device.name.isEmpty ? 'Unknown Device' : data.device.name),
-                                subtitle: Text(data.device.id.id),
-                                trailing: Text(data.rssi.toString()),
-                              ),
-                            );
-                          },
-                        );
-                      } else if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const CircularProgressIndicator();
-                      } else {
-                        return const Center(child: Text("No Device Found"));
-                      }
-                    },
-                  ),
+                  child: Obx(() {
+                    if (controller.scannedDevices.isNotEmpty) {
+                      return ListView.builder(
+                        itemCount: controller.scannedDevices.length,
+                        itemBuilder: (context, index) {
+                          final data = controller.scannedDevices[index];
+                          return Card(
+                            elevation: 2,
+                            child: ListTile(
+                              title: Text(data.device.name.isEmpty ? 'Unknown Device' : data.device.name),
+                              subtitle: Text(data.device.id.id),
+                              trailing: Text(data.rssi.toString()),
+                            ),
+                          );
+                        },
+                      );
+                    } else {
+                      return const Center(child: Text("No Device Found"));
+                    }
+                  }),
                 ),
                 const SizedBox(height: 10),
                 ElevatedButton(
